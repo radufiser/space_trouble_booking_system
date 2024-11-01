2022e1bb1cc02f9cda7bcf39c927298cb2df40b3
Rad Fiser <radu.fiser@gmail.com>
2024-11-01 11:07:08 +0100

    add launchpads, destination and schedule tables

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,28 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
+CREATE TABLE IF NOT EXISTS destinations (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS launchpads (
+    id VARCHAR(36) PRIMARY KEY,
+    name TEXT NOT NULL,
+    full_name TEXT NOT NULL,
+    locality TEXT,
+    region TEXT,
+    status TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS weekly_schedule (
+    launchpad_id TEXT,
+    day_of_week INT,                   -- 0 for Monday, 1 for Tuesday
+    destination_id UUID REFERENCES destinations(id) ON DELETE CASCADE,
+    last_updated TIMESTAMPTZ,           
+    PRIMARY KEY (launchpad_id, day_of_week)
+);
+
 
 CREATE TABLE IF NOT EXISTS bookings (
     id VARCHAR(36) PRIMARY KEY,
@@ -9,3 +34,13 @@ CREATE TABLE IF NOT EXISTS bookings (
     destination_id VARCHAR(20) NOT NULL,
     launch_date TIMESTAMP NOT NULL
 );
+
+INSERT INTO destinations (name) VALUES
+('Mars'),
+('Moon'),
+('Pluto'),
+('Asteroid Belt'),
+('Europa'),
+('Titan'),
+('Ganymede')
+ON CONFLICT (name) DO NOTHING;
